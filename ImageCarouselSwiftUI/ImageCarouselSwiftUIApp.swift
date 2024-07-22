77893b659282888c5287a07530edebed01ed7c8b
Ganesh Patil <ganesh.p@neosoftmail.com>
2024-07-22 15:30:18 +0530

    Initial Commit

---
+++ ImageCarouselSwiftUI/ImageCarouselSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCarouselSwiftUIApp.swift
+//  ImageCarouselSwiftUI
+//
+//  Created by Neosoft on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageCarouselSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
